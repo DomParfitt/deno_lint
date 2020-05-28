a5fe106eb18a33416e54b83e48937648078d8d1b
Dom Parfitt <mail@domparfitt.com>
2020-05-28 22:04:33 +0100

    Updated rule

---
+++ src/rules/no_fallthrough.rs
@@ -0,0 +1,50 @@
+use super::{Context, LintRule};
+use swc_ecma_ast::Stmt::{Break, Continue, Return, Throw};
+use swc_ecma_ast::{Module, Stmt, SwitchStmt};
+use swc_ecma_visit::{Node, Visit};
+
+pub struct NoFallthrough;
+
+impl LintRule for NoFallthrough {
+  fn new() -> Box<Self> {
+    Box::new(NoFallthrough)
+  }
+
+  fn lint_module(&self, context: Context, module: Module) {
+    let mut visitor = NoFallthroughVisitor::new(context);
+    visitor.visit_module(&module, &module);
+  }
+}
+
+pub struct NoFallthroughVisitor {
+  context: Context,
+}
+
+impl NoFallthroughVisitor {
+  pub fn new(context: Context) -> Self {
+    Self { context }
+  }
+}
+
+impl Visit for NoFallthroughVisitor {
+  fn visit_switch_stmt(&mut self, switch: &SwitchStmt, _parent: &dyn Node) {
+    for case in &switch.cases {
+      if case.cons.iter().any(|stmt| is_control_flow_stmt(stmt)) {
+        continue;
+      }
+
+      self.context.add_diagnostic(
+        case.span,
+        "noFallthrough",
+        "Expected break statement before case",
+      )
+    }
+  }
+}
+
+fn is_control_flow_stmt(stmt: &Stmt) -> bool {
+  match stmt {
+    Return(_) | Break(_) | Continue(_) | Throw(_) => true,
+    _ => false,
+  }
+}
